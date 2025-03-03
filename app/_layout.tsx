0423430a904dc943d668c10ee9d2bbc8819042db
Sarwar-Shawon <sshawon95@gmail.com>
2025-03-03 23:09:18 +0000

    setup the home page and routing

---
+++ app/_layout.tsx
@@ -0,0 +1,5 @@
+import "../global.css";
+import { Slot, Stack } from "expo-router";
+export default function RootLayout() {
+  return <Slot />;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,42 @@
-import "../global.css";
-import { View, Text, ImageBackground } from "react-native";
+// import "../global.css";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
 import beachImage from "@/assets/meditation-images/beach.webp";
+import CustomButton from "@/components/CustomButton";
+//
 const App = () => {
   return (
-    <View className="flex-1 justify-center items-center">
-      {/* <ImageBackground
+    <View className="flex-1">
+      <ImageBackground
         source={beachImage}
         resizeMode="cover"
         className="flex-1"
-      > */}
-      <Text>App</Text>
-      {/* </ImageBackground> */}
+      >
+        <LinearGradient
+          style={{ flex: 1 }}
+          colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+        >
+          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                App
+              </Text>
+              <Text className="text-center text-white text-regular text-2xl mt-3">
+                Simplifying Meditation for Everyone
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                onPress={() => console.log("tap")}
+                title={"Get Started"}
+              />
+            </View>
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 };
