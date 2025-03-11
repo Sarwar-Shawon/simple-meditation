2a38071483289b3b5835593562238a3717d52545
Sarwar-Shawon <sshawon95@gmail.com>
2025-03-11 00:14:52 +0000

    setup meditation page

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  Pressable,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { GalleryPrivewData } from "@/constants/models/AffirmationCategory";
 import AFFIRMATION_GALLERY from "@/constants/affirmation-gallary";
+import AppGradient from "@/components/AppGradient";
+import { AntDesign } from "@expo/vector-icons";
 
 const AffirmationPractice = () => {
   const { itemId } = useLocalSearchParams();
   const [affirmation, setAffirmation] = useState<GalleryPrivewData>();
+  const [affirmationText, setAffirmationText] = useState<string[]>([]);
 
   useEffect(() => {
     for (let idx = 0; idx < AFFIRMATION_GALLERY.length; idx++) {
@@ -16,14 +25,44 @@ const AffirmationPractice = () => {
       );
       if (affirmationToStart) {
         setAffirmation(affirmationToStart);
+        const affirmationTextArray = affirmationToStart.text.split(".");
+        if (affirmationTextArray[affirmationTextArray.length - 1] == "") {
+          affirmationTextArray.pop();
+        }
+        setAffirmationText(affirmationTextArray);
         return;
       }
     }
   }, []);
 
   return (
-    <View>
-      <Text>AffirmationPractice</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={affirmation?.image}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0,0,0,0.3)", "rgba(0,0,0,0.8)"]}>
+          <Pressable
+            onPress={() => router.back()}
+            className="absolute top-16 left-6"
+          >
+            <AntDesign name="leftcircleo" size={50} color={"white"} />
+          </Pressable>
+          <ScrollView className="mt-20" showsVerticalScrollIndicator={false}>
+            <View className="h-full justify-center">
+              {affirmationText.map((text, index) => (
+                <Text
+                  key={index}
+                  className="text-white text-center text-3xl font-bold mb-8"
+                >
+                  {text}.
+                </Text>
+              ))}
+            </View>
+          </ScrollView>
+        </AppGradient>
+      </ImageBackground>
     </View>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,38 @@
 import "../global.css";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync(); // Prevent the splash screen from hiding until fonts are loaded
+
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync(); // Hide splash screen when fonts are loaded
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; // Don't render anything until the fonts are loaded
+  }
+  if (!fontsLoaded && !error) {
+    return null; // Don't render anything until the fonts are loaded
+  }
   return (
     <Stack>
-        <Stack.Screen
+      <Stack.Screen
         name="(tabs)"
         options={{ headerShown: false }}
       ></Stack.Screen>
       <Stack.Screen
         name="index"
+        options={{ headerShown: false }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="meditate/[id]"
         options={{ headerShown: false }}
       ></Stack.Screen>
     </Stack>
